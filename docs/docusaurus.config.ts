e523da955914e1d922a0e605501a34cbb0bd9ba1
ExecuteAutomation <kartmcad@gmail.com>
2024-12-17 12:11:22 +1300

    ignore the broken link warning

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   organizationName: 'executeautomation', // Usually your GitHub org/user name.
   projectName: 'Playwright-MCP-Server', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
@@ -43,21 +43,6 @@ const config: Config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   feedOptions: {
-        //     type: ['rss', 'atom'],
-        //     xslt: true,
-        //   },
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        //   // Useful options to enforce blogging best practices
-        //   onInlineTags: 'warn',
-        //   onInlineAuthors: 'warn',
-        //   onUntruncatedBlogPosts: 'warn',
-        // },
         theme: {
           customCss: './src/css/custom.css',
         },
